d18abc6d8c077643a2d07a01d8fd259a8a0ce7a7
Andrii Lutsiak <timechain04@gmail.com>
2023-08-02 14:04:29 +0300

    private-log

--- src/сomponents/Routes/PublicRoute.js
+++ src/сomponents/Routes/PublicRoute.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { getLoggedin } from 'redux/slice/AuthSlise';
+import PropTypes from 'prop-types'; 
 
 const PublicRoute = ({ children, restricted = false }) => {
   const isLoggedin = useSelector(getLoggedin);
   const shouldRedirect = isLoggedin && restricted;
   return shouldRedirect ? <Navigate to="/contacts" /> : children;
+};
+
+PublicRoute.propTypes = {
+  children: PropTypes.node.isRequired, 
+  restricted: PropTypes.bool, 
 };
 
 export default PublicRoute;
